40b6559c78166555b9ec6631d4f3b5c9145cc5c0
Shoxjaxon1202 <shoxjaxonabdusalimov1202@gmail.com>
2024-11-20 14:31:45 +0500

    rasm

--- Client/src/constants/db.js
+++ Client/src/constants/db.js
@@ -3,29 +3,25 @@ export function getData() {
     {
       title: "JavaScript",
       price: 17.99,
-      Image:
-        "https://sammi.zc/image?url=https%3A%2Fmedia.graphassets.com%2F3gf746AKRbWNj880C0EB&w=3840&q=75",
+      Image: "https://www.sammi.ac/logo.svg",
       id: 1,
     },
     {
       title: "React",
       price: 17.99,
-      Image:
-        "https://sammi.zc/image?url=https%3A%2Fmedia.graphassets.com%2F3gf746AKRbWNj880C0EB&w=3840&q=75",
+      Image: "https://www.sammi.ac/logo.svg",
       id: 2,
     },
     {
       title: "Angular",
       price: 17.99,
-      Image:
-        "https://sammi.zc/image?url=https%3A%2Fmedia.graphassets.com%2F3gf746AKRbWNj880C0EB&w=3840&q=75",
+      Image: "https://www.sammi.ac/logo.svg",
       id: 3,
     },
     {
       title: "Vite",
       price: 17.99,
-      Image:
-        "https://sammi.zc/image?url=https%3A%2Fmedia.graphassets.com%2F3gf746AKRbWNj880C0EB&w=3840&q=75",
+      Image: "https://www.sammi.ac/logo.svg",
       id: 4,
     },
   ];
